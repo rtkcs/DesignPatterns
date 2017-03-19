4e2d8469e11c4b331e9d4a59d8db34a1a9207f7d
Takács Róbert <rtakacsux@takacsubuntb2.ux.gratex.com>
2017-03-19 03:20:58 +0100

    MVC design pattern

---
+++ MVC/src/eu/rtakacs/mvc/MVCCalculator.java
@@ -0,0 +1,11 @@
+package eu.rtakacs.mvc;
+
+public class MVCCalculator {
+
+	public static void main(String[] args) {
+		CalculatorView view = new CalculatorView();
+		CalculatorModel model = new CalculatorModel();
+		CalculatorController controller = new CalculatorController(view, model);
+		view.setVisible(true);
+	}
+}
